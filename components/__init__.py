f3c3c8858e0beaa697957bc52a9ce128b4bf047d
C V VISHNU VARDHAN  CHARY <vishnuvardhancv128@gmail.com>
2025-08-10 18:12:57 +0000

    Upload New File

---
+++ components/__init__.py
@@ -0,0 +1,9 @@
+# components/__init__.py
+
+from . import home
+from . import Assistant
+from . import record_keeping
+from . import disease_detection
+from . import weather_crop_planner
+from . import profit_calculator
+from . import crop_suggestion
